05ce3fd8feadb452eb40cdbfd5571a73dd38ff0c
DeveloopClick <develoop.click@gmail.com>
2023-06-18 21:30:06 +0300

    z

--- VirtualMemory.cpp
+++ VirtualMemory.cpp
@@ -8,7 +8,6 @@
 
 #define ROOT_TABLE_PAGE 0
 
-
 uint64_t get_offset (uint64_t virtualAddress, uint64_t level)
 {
   uint64_t shift = (VIRTUAL_ADDRESS_WIDTH - ((level + 1) * OFFSET_WIDTH));
@@ -22,13 +21,13 @@ void clear (uint64_t frameIndex)
     PMwrite (frameIndex * PAGE_SIZE + i, 0);
   }
 }
-bool is_empty_frame(uint64_t frameIndex)
+bool is_empty_frame (uint64_t frameIndex)
 {
   word_t value;
   for (uint64_t i = 0; i < PAGE_SIZE; i++)
   {
     PMread (frameIndex * PAGE_SIZE + i, &value);
-    if (value!=0)
+    if (value != 0)
     {
       return false;
     }
@@ -38,10 +37,12 @@ bool is_empty_frame(uint64_t frameIndex)
 /*
  * min{NUM_PAGES - |page_swapped_in - p|, |page_swapped_in - p|}
  */
-uint64_t cyclicDistance(uint64_t page_swapped_in, uint64_t p) {
-  uint64_t distance1 = NUM_PAGES - std::abs(static_cast<int64_t>(page_swapped_in - p));
-  uint64_t distance2 = std::abs(static_cast<int64_t>(page_swapped_in - p));
-  return std::min(distance1, distance2);
+uint64_t cyclicDistance (uint64_t page_swapped_in, uint64_t p)
+{
+  uint64_t distance1 =
+      NUM_PAGES - std::abs (static_cast<int64_t>(page_swapped_in - p));
+  uint64_t distance2 = std::abs (static_cast<int64_t>(page_swapped_in - p));
+  return std::min (distance1, distance2);
 }
 
 word_t get_address (uint64_t virtualAddress)
@@ -55,24 +56,36 @@ word_t get_address (uint64_t virtualAddress)
     PMread (page_index * PAGE_SIZE + offset, &value);
     if (value == 0)
     {
-      // Find an unused frame or evict a page
-      uint64_t frameIndex;
-      for (frameIndex = 1; frameIndex <= NUM_FRAMES; frameIndex++)
+      //In order to create a new table, we need a frame. We find one by traversing the entire tree in DFS.
+      //This is done by going to the root table (always in frame 0), iterating over its rows, and recursively entering every entry that isn’t 0.
+      //This time the traversal we only saw one frame during the traversal – 0, therefore we know that frame 1 is unused
+      uint64_t frameIndex = 0;
+      uint64_t min_distance = NUM_PAGES;
+      for (uint64_t j = 0; j < NUM_FRAMES; j++)
       {
-        word_t frameData;
-        PMread(frameIndex * PAGE_SIZE, &frameData);
-        if (frameData == 0) {
+        if (is_empty_frame (j))
+        {
+          frameIndex = j;
           break;
+        }
+        word_t page_swapped_in;
+        PMread (j * PAGE_SIZE, &page_swapped_in);
+        uint64_t distance = cyclicDistance (page_swapped_in, i);
+        if (distance < min_distance)
+        {
+          min_distance = distance;
+          frameIndex = j;
         }
       }
 
       // Clear the frame if next layer is a table
-      if (i < TABLES_DEPTH - 1) {
-        clear(frameIndex);
+      if (i < TABLES_DEPTH - 1)
+      {
+        clear (frameIndex);
       }
 
       // Update the parent table with the new frame index
-      PMwrite((page_index * PAGE_SIZE) + offset, frameIndex);
+      PMwrite ((page_index * PAGE_SIZE) + offset, frameIndex);
       page_index = frameIndex;
     }
     else
@@ -83,9 +96,6 @@ word_t get_address (uint64_t virtualAddress)
   }
   return page_index * PAGE_SIZE + offset;
 }
-
-
-
 
 /*
  * Initialize the virtual memory.
