6dacfec2cf149d8401c181bc1f45d8892724b7ab
DeveloopClick <develoop.click@gmail.com>
2023-06-19 12:26:27 +0300

    z

--- VirtualMemory.cpp
+++ VirtualMemory.cpp
@@ -9,11 +9,6 @@
 
 #define ROOT_TABLE_PAGE 0
 
-uint64_t get_offset (uint64_t virtualAddress, uint64_t level)
-{
-  uint64_t shift = (VIRTUAL_ADDRESS_WIDTH - ((level + 1) * OFFSET_WIDTH));
-  return (virtualAddress >> shift) & ((1 << OFFSET_WIDTH) - 1);
-}
 
 void clear (uint64_t frameIndex)
 {
@@ -22,6 +17,7 @@ void clear (uint64_t frameIndex)
     PMwrite (frameIndex * PAGE_SIZE + i, 0);
   }
 }
+
 bool is_empty_frame (uint64_t frameIndex)
 {
   word_t value;
@@ -36,88 +32,125 @@ bool is_empty_frame (uint64_t frameIndex)
   return true;
 }
 
-// get page by frame
-uint64_t get_page_by_frame (uint64_t frameIndex)
-{
-  word_t value;
-  PMread (frameIndex * PAGE_SIZE, &value);
-  return value;
-}
-
-// get parent of frame
-uint64_t get_parent (uint64_t frameIndex, uint64_t level)
-{
-  uint64_t page = get_page_by_frame (frameIndex);
-  uint64_t offset = get_offset (page, level);
-  return page - offset;
-}
-
-//
-
-/*
- * min{NUM_PAGES - |page_swapped_in - p|, |page_swapped_in - p|}
- */
 uint64_t cyclicDistance (uint64_t page_swapped_in, uint64_t p)
 {
-  uint64_t distance1 =
-      NUM_PAGES - std::abs (static_cast<int64_t>(page_swapped_in - p));
+  uint64_t distance1 = NUM_PAGES - std::abs (static_cast<int64_t>
+      (page_swapped_in - p));
   uint64_t distance2 = std::abs (static_cast<int64_t>(page_swapped_in - p));
   return std::min (distance1, distance2);
 }
 
-word_t translate_address (uint64_t virtualAddress)
+word_t get_parent_of_frame (word_t root ,word_t frameIndex)
 {
-
-  uint64_t addr[TABLES_DEPTH];
-  uint64_t virtual_without_offset = virtualAddress >> OFFSET_WIDTH;
-  for (uint64_t i = TABLES_DEPTH - 1; i <= 0; i--)
+ // dfs on the tree and find the parent of the frame
+  word_t value;
+  for(uint64_t i = 0; i < PAGE_SIZE; i++)
   {
-      addr[i] = virtual_without_offset & ((1 << OFFSET_WIDTH) - 1);
-      virtual_without_offset = virtual_without_offset >> OFFSET_WIDTH;
-  }
 
-  uint64_t offset = 0;
-  uint64_t page_index = 0;
-  for (uint64_t i = 0; i < TABLES_DEPTH; i++)
-  {
-    word_t value;
-    offset = get_offset (virtualAddress, i);
-    PMread (page_index * PAGE_SIZE + offset, &value);
-    if (value == 0)
+    PMread (root * PAGE_SIZE+i, &value);
+    if (value == frameIndex)
     {
-      uint64_t frameIndex = 0;
-      if (frameIndex == 0)
-      {
-        uint64_t min_distance = NUM_PAGES;
-        uint64_t min_distance_frame = 0;
-        for (uint64_t j = 0; j < NUM_FRAMES; j++)
-        {
-          uint64_t parent = get_parent (j, i);
-          if (parent == page_index)
-          {
-            uint64_t page = get_page_by_frame (j);
-            uint64_t distance = cyclicDistance (page, page_index);
-            if (distance < min_distance)
-            {
-              min_distance = distance;
-              min_distance_frame = j;
-            }
-          }
-        }
-        frameIndex = min_distance_frame;
-        uint64_t page = get_page_by_frame (frameIndex);
-        clear (frameIndex);
-        PMwrite (frameIndex * PAGE_SIZE, page);
-      }
-      PMwrite (page_index * PAGE_SIZE + offset, frameIndex);
+      return root;
     }
     else
     {
-      page_index = value;
+      return get_parent_of_frame (value, frameIndex);
+    }
+  }
+}
+
+word_t get_unused_or_empty_frame (word_t justCreatedFrame)
+{
+  word_t frameIndex = 0;
+
+  ...
+  if (frameIndex != justCreatedFrame && is_empty_frame (frameIndex))
+  {
+    return frameIndex;
+  }
+  ...
+
+
+  if (frameIndex == NUM_FRAMES - 1)
+  {
+    return 0;
+  }
+  else
+  {
+    return frameIndex + 1;
+  }
+}
+
+// get_page_of_max_dist
+word_t get_page_of_max_dist (uint64_t virtualAddress)
+{
+  word_t maxPage = 0;
+  word_t maxDist = 0;
+  for (word_t i = 0; i < NUM_PAGES; i++)
+  {
+    word_t dist = cyclicDistance (i, virtualAddress);
+    if (dist > maxDist)
+    {
+      maxDist = dist;
+      maxPage = i;
+    }
+  }
+  return maxPage;
+}
+
+// get_frame_of_page
+word_t get_frame_of_page (word_t page)
+{
+  word_t frameIndex;
+  for (frameIndex = 0; frameIndex < NUM_FRAMES; frameIndex++)
+  {
+    word_t value;
+    PMread (frameIndex * PAGE_SIZE, &value);
+    if (value == page)
+    {
+      break;
+    }
+  }
+  return frameIndex;
+}
+
+word_t translate_address (uint64_t virtualAddress)
+{
+  uint64_t addr[TABLES_DEPTH + 1];
+  uint64_t left_virtual_address = virtualAddress;
+  for (uint64_t i = TABLES_DEPTH; i <= 0; i--)
+  {
+    addr[i] = left_virtual_address & ((1 << OFFSET_WIDTH) - 1);
+    left_virtual_address = left_virtual_address >> OFFSET_WIDTH;
+  }
+
+  word_t nextIndex = ROOT_TABLE_PAGE;
+  word_t pageIndex = ROOT_TABLE_PAGE;
+  word_t preFrameIndex = ROOT_TABLE_PAGE;
+  for (uint64_t i = 0; i < TABLES_DEPTH; i++)
+  {
+    PMread (pageIndex * PAGE_SIZE + addr[i], &nextIndex);
+    if (nextIndex == 0)
+    {
+      word_t frameIndex = get_unused_or_empty_frame (preFrameIndex);
+      preFrameIndex = frameIndex;
+      if (frameIndex == 0) // all frames are used
+      {
+        // find the page with the max distance from the current page
+        word_t maxPage = get_page_of_max_dist (virtualAddress >> OFFSET_WIDTH);
+        frameIndex = get_frame_of_page (maxPage);
+        // disconnect from previous parent
+        PMevict (frameIndex, maxPage);
+        PMwrite (get_parent_of_frame (frameIndex), 0);
+      }
+      clear (frameIndex);
+      PMwrite (pageIndex * PAGE_SIZE + addr[i], frameIndex);
     }
 
+    pageIndex = nextIndex;
   }
-  return page_index * PAGE_SIZE + offset;
+  PMrestore (pageIndex, virtualAddress >> OFFSET_WIDTH);
+  return pageIndex * PAGE_SIZE + addr[TABLES_DEPTH];
 }
 
 /*
