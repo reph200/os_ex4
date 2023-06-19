07e08c8f5ceae5c0eca963901e17e60e830aaada
DeveloopClick <develoop.click@gmail.com>
2023-06-19 10:08:18 +0300

    z

--- VirtualMemory.cpp
+++ VirtualMemory.cpp
@@ -5,6 +5,7 @@
 #include "PhysicalMemory.h"
 #include <cstdint>
 #include <algorithm>
+#include <cmath>
 
 #define ROOT_TABLE_PAGE 0
 
@@ -34,6 +35,25 @@ bool is_empty_frame (uint64_t frameIndex)
   }
   return true;
 }
+
+// get page by frame
+uint64_t get_page_by_frame (uint64_t frameIndex)
+{
+  word_t value;
+  PMread (frameIndex * PAGE_SIZE, &value);
+  return value;
+}
+
+// get parent of frame
+uint64_t get_parent (uint64_t frameIndex, uint64_t level)
+{
+  uint64_t page = get_page_by_frame (frameIndex);
+  uint64_t offset = get_offset (page, level);
+  return page - offset;
+}
+
+//
+
 /*
  * min{NUM_PAGES - |page_swapped_in - p|, |page_swapped_in - p|}
  */
@@ -45,8 +65,17 @@ uint64_t cyclicDistance (uint64_t page_swapped_in, uint64_t p)
   return std::min (distance1, distance2);
 }
 
-word_t get_address (uint64_t virtualAddress)
+word_t translate_address (uint64_t virtualAddress)
 {
+
+  uint64_t addr[TABLES_DEPTH];
+  uint64_t virtual_without_offset = virtualAddress >> OFFSET_WIDTH;
+  for (uint64_t i = TABLES_DEPTH - 1; i <= 0; i--)
+  {
+      addr[i] = virtual_without_offset & ((1 << OFFSET_WIDTH) - 1);
+      virtual_without_offset = virtual_without_offset >> OFFSET_WIDTH;
+  }
+
   uint64_t offset = 0;
   uint64_t page_index = 0;
   for (uint64_t i = 0; i < TABLES_DEPTH; i++)
@@ -56,37 +85,31 @@ word_t get_address (uint64_t virtualAddress)
     PMread (page_index * PAGE_SIZE + offset, &value);
     if (value == 0)
     {
-      //In order to create a new table, we need a frame. We find one by traversing the entire tree in DFS.
-      //This is done by going to the root table (always in frame 0), iterating over its rows, and recursively entering every entry that isn’t 0.
-      //This time the traversal we only saw one frame during the traversal – 0, therefore we know that frame 1 is unused
       uint64_t frameIndex = 0;
-      uint64_t min_distance = NUM_PAGES;
-      for (uint64_t j = 0; j < NUM_FRAMES; j++)
+      if (frameIndex == 0)
       {
-        if (is_empty_frame (j))
+        uint64_t min_distance = NUM_PAGES;
+        uint64_t min_distance_frame = 0;
+        for (uint64_t j = 0; j < NUM_FRAMES; j++)
         {
-          frameIndex = j;
-          break;
+          uint64_t parent = get_parent (j, i);
+          if (parent == page_index)
+          {
+            uint64_t page = get_page_by_frame (j);
+            uint64_t distance = cyclicDistance (page, page_index);
+            if (distance < min_distance)
+            {
+              min_distance = distance;
+              min_distance_frame = j;
+            }
+          }
         }
-        word_t page_swapped_in;
-        PMread (j * PAGE_SIZE, &page_swapped_in);
-        uint64_t distance = cyclicDistance (page_swapped_in, i);
-        if (distance < min_distance)
-        {
-          min_distance = distance;
-          frameIndex = j;
-        }
-      }
-
-      // Clear the frame if next layer is a table
-      if (i < TABLES_DEPTH - 1)
-      {
+        frameIndex = min_distance_frame;
+        uint64_t page = get_page_by_frame (frameIndex);
         clear (frameIndex);
+        PMwrite (frameIndex * PAGE_SIZE, page);
       }
-
-      // Update the parent table with the new frame index
-      PMwrite ((page_index * PAGE_SIZE) + offset, frameIndex);
-      page_index = frameIndex;
+      PMwrite (page_index * PAGE_SIZE + offset, frameIndex);
     }
     else
     {
@@ -114,7 +137,7 @@ void VMinitialize ()
  */
 int VMread (uint64_t virtualAddress, word_t *value)
 {
-  uint64_t address = get_address (virtualAddress);
+  uint64_t address = translate_address (virtualAddress);
   if (address == 0)
   {
     return 0;
@@ -131,7 +154,7 @@ int VMread (uint64_t virtualAddress, word_t *value)
  */
 int VMwrite (uint64_t virtualAddress, word_t value)
 {
-  uint64_t address = get_address (virtualAddress);
+  uint64_t address = translate_address (virtualAddress);
   if (address == 0)
   {
     return 0;
